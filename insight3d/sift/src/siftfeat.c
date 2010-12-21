50b32ac7386ec6eed7289f6c5d375aa899ef2008
Tobias Jakobs <Tobias.Jakobs@googlemail.com>
2010-12-21 22:56:27 +0100

    added 3. argument to cvSaveImage

--- insight3d/sift/src/siftfeat.c
+++ insight3d/sift/src/siftfeat.c
@@ -75,7 +75,7 @@ int main( int argc, char** argv )
     export_features( out_file_name, features, n );
 
   if( out_img_name != NULL )
-    cvSaveImage( out_img_name, img );
+    cvSaveImage( out_img_name, img, 0 );
   return 0;
 }
 
